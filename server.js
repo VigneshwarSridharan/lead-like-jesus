f9e2d5e7d13119c43c4f8e7cfc76b1d44f29d7ba
Vigneshwar <vigneshwar.s>
2020-07-09 19:39:45 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-// app.prepare().then(() => {
+app.prepare().then(() => {
   const server = express()
   server.use(cors())
   server.use(express.json());
