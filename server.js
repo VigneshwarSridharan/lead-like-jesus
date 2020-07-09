d2e206b1943c1ab0efe987db099f41461cf14f20
Vigneshwar <vigneshwar.s>
2020-07-09 19:30:06 +0530

    changes

--- server.js
+++ server.js
@@ -7,28 +7,28 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
+// app.prepare().then(() => {
   const server = express()
   server.use(express.json());
   server.use(express.urlencoded({ extended: false }));
   server.use(express.static('./public'))
   server.use('/api', APIRouters)
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
+  // server.get('/a', (req, res) => {
+  //   return app.render(req, res, '/a', req.query)
+  // })
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
+  // server.get('/b', (req, res) => {
+  //   return app.render(req, res, '/b', req.query)
+  // })
 
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  // server.all('*', (req, res) => {
+  //   return handle(req, res)
+  // })
 
   server.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
-})
+// })
