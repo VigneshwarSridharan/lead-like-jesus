edec702128cf8428e6934a406efd81c02f19e283
Vigneshwar <vigneshwar.s>
2020-07-17 13:59:09 +0530

    dashboard added

--- components/dashboard/Sideboar.js
+++ components/dashboard/Sideboar.js
@@ -3,6 +3,7 @@ import { ListGroup, ListGroupItem } from "reactstrap"
 import { useRouter } from "next/router";
 
 let sidebardata = [
+    
     {
         name: 'Users',
         icon: 'fas fa-user',
@@ -21,10 +22,13 @@ const Sidebar = props => {
     const goToPath = path => router.push(path)
     return (
         <div className="sidebar">
-            <Link href="/">
+            <Link href="/dashboard">
                 <div className="h4 text-center pointer  p-3 mb-5">Lead Like Jesus</div>
             </Link>
             <ListGroup flush className="border-top border-bottom" >
+                <ListGroupItem action tag="a" className={`${'/dashboard' == router.pathname ? 'active' : ''}`} onClick={() => goToPath('/dashboard')}>
+                    <i className={`fas fa-layer-group mr-2`}></i> Dashboard
+                </ListGroupItem>
                 {
                     sidebardata.map((item, inx) => {
                         return (
